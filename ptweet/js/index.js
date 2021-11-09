f956bb8679b6b5565d6091385b335b3acef6482d
Cesar Carmona <cesrafcata@gmail.com>
2021-11-09 01:06:22 -0700

    Tweets chidos

--- ptweet/js/index.js
+++ ptweet/js/index.js
@@ -1,5 +1,43 @@
 window.onload=()=>{
     let usuario=JSON.parse(localStorage.getItem("usuario"));
-    console.log(usuario);
+    let arrayTweets=[]
     document.getElementById("usuario").innerHTML="Hola, "+usuario.nombre;
+    document.getElementById("imgUser").src='./img/'+usuario.imagen;
+    let txtTweet=document.getElementById("txtTweet");
+    let btnTweet=document.getElementById("btnTweet");
+    btnTweet.addEventListener('click',(e)=>{
+        if(txtTweet.value.trim()!=""){
+            let obj={
+                img:usuario.imagen,
+                nombre:usuario.nombre,
+                mensaje:txtTweet.value,
+                username:usuario.username,
+            };
+            arrayTweets.push(obj);
+            crearPost();
+        }
+    });
+    txtTweet.addEventListener('keyup',(e)=>{
+
+    });
+    function crearPost(){
+        var todo="";
+        arrayTweets.forEach(tweet=>{
+            todo+=`<div class="post">
+            <div>
+                <img src="img/${tweet.img}" alt="" class="imgUser">
+            </div>
+            <div>
+                <h2> 
+                    <span>${tweet.nombre}</span>
+                    <span>@${tweet.username}</span>
+                </h2>
+                <div class="textTweet">
+                    ${tweet.mensaje}
+                </div>
+            </div>
+        </div>`;
+        });
+        document.getElementById("posts").innerHTML=todo;
+    }
 }
